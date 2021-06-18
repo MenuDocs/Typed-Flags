5c843555316500dfd11ece8614eb7c94ab179cc4
skelmis <skelmis.craft@gmail.com>
2021-06-18 21:39:11 +1200

    Push new version

--- typed_flags/__init__.py
+++ typed_flags/__init__.py
@@ -1,10 +1,10 @@
 from .typed_flags import TypedFlags
 
-__version__ = "1.0.0"
+__version__ = "1.1.0"
 
 import logging
 from collections import namedtuple
 
 logging.getLogger(__name__).addHandler(logging.NullHandler())
 VersionInfo = namedtuple("VersionInfo", "major minor micro releaselevel serial")
-version_info = VersionInfo(major=1, minor=0, micro=0, releaselevel="final", serial=0)
+version_info = VersionInfo(major=1, minor=1, micro=0, releaselevel="final", serial=0)
