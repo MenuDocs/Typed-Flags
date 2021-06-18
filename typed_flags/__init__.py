e672dc7225c6761a5d30f7bb1092053fad1097ad
skelmis <skelmis.craft@gmail.com>
2021-06-18 21:42:30 +1200

    Bump version

--- typed_flags/__init__.py
+++ typed_flags/__init__.py
@@ -1,10 +1,11 @@
 from .typed_flags import TypedFlags
+from .exceptions import ReservedKeyword
 
-__version__ = "1.1.0"
+__version__ = "1.1.1"
 
 import logging
 from collections import namedtuple
 
 logging.getLogger(__name__).addHandler(logging.NullHandler())
 VersionInfo = namedtuple("VersionInfo", "major minor micro releaselevel serial")
-version_info = VersionInfo(major=1, minor=1, micro=0, releaselevel="final", serial=0)
+version_info = VersionInfo(major=1, minor=1, micro=1, releaselevel="final", serial=0)
