d93e10b938751fd2ca836390c039c4ce2b998e2a
skelmis <skelmis.craft@gmail.com>
2021-06-18 21:11:22 +1200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,35 @@
+import re
+
+import setuptools
+
+with open("readme.md", "r") as fh:
+    long_description = fh.read()
+
+_version_regex = (
+    r"^__version__ = ('|\")((?:[0-9]+\.)*[0-9]+(?:\.?([a-z]+)(?:\.?[0-9])?)?)\1$"
+)
+
+with open("typed_flags/__init__.py") as stream:
+    match = re.search(_version_regex, stream.read(), re.MULTILINE)
+
+version = match.group(2)
+
+setuptools.setup(
+    name="typed-flags",
+    version=version,
+    author="Menudocs",
+    author_email="contact@menudocs.org",
+    description="A Typehint based flags system for discord.py",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/MenuDocs/Typed-Flags",
+    packages=setuptools.find_packages(),
+    install_requires=["discord.py>=1"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Development Status :: 5 - Production/Stable",
+    ],
+    python_requires=">=3.6",
+)
--- typed_flags/__init__.py
+++ typed_flags/__init__.py
@@ -1,1 +1,10 @@
 from .typed_flags import TypedFlags
+
+__version__ = "1.0.0"
+
+import logging
+from collections import namedtuple
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
+VersionInfo = namedtuple("VersionInfo", "major minor micro releaselevel serial")
+version_info = VersionInfo(major=1, minor=0, micro=0, releaselevel="final", serial=0)
